285564d1ee14ca96269f728c041a4bfa2ac9ef75
youssef <youss4923@gmail.com>
2025-08-03 22:24:48 +0300

    permissions

---
+++ accounts/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+
+class IsStudent(BasePermission):
+    """
+    Allows access only to users with user_type == 'student'.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.user_type == 'student'
+
+
+
+class IsStudent(BasePermission):
+    """
+    Allows access only to users with user_type == 'teacher'.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.user_type == 'teacher'
